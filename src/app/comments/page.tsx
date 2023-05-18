d641b52e6efa00159135b2c22ad2dd3dcd011b9a
joaovictor09 <joaovictordasilva0911@gmail.com>
2023-05-17 21:34:51 -0300

    fix: leaking vercel

--- src/app/comments/page.tsx
+++ src/app/comments/page.tsx
@@ -1,6 +1,28 @@
+import { z } from 'zod'
 import { Comment } from './components/Comment'
 import { CommentForm } from './components/CommentForm'
-import { getComments } from '@/lib/getComments'
+
+const commentsSchema = z
+  .object({
+    id: z.string().nonempty(),
+    user: z.string().nonempty(),
+    comment: z.string().nonempty(),
+  })
+  .array()
+
+export async function getComments() {
+  const res = await fetch('http://localhost:3000/api/comments', {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = commentsSchema.parse(await res.json())
+
+  return data
+}
 
 export default async function Comments() {
   const comments = await getComments()
