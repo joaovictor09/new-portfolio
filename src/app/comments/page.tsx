c41ea136e9272618176985f4fde8a87924622f12
joaovictor09 <joaovictordasilva0911@gmail.com>
2023-05-17 21:57:59 -0300

    test: remove zod parse

--- src/app/comments/page.tsx
+++ src/app/comments/page.tsx
@@ -1,49 +1,51 @@
-// import { z } from 'zod'
+import { z } from 'zod'
 import { Comment } from './components/Comment'
 import { CommentForm } from './components/CommentForm'
 
-// const commentsSchema = z
-//   .object({
-//     id: z.string().nonempty(),
-//     user: z.string().nonempty(),
-//     comment: z.string().nonempty(),
-//   })
-//   .array()
+const commentsSchema = z
+  .object({
+    id: z.string().nonempty(),
+    user: z.string().nonempty(),
+    comment: z.string().nonempty(),
+  })
+  .array()
+
+type commentsProps = z.infer<typeof commentsSchema>
+
+async function getComments() {
+  const res = await fetch('http://localhost:3000/api/comments', {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  // const data = commentsSchema.parse(await res.json())
+
+  return res.json()
+}
 
 export default async function Comments() {
-  // async function getComments() {
-  //   const res = await fetch('http://localhost:3000/api/comments', {
-  //     cache: 'no-store',
-  //   })
+  const comments: commentsProps = await getComments()
 
-  //   if (!res.ok) {
-  //     throw new Error('Failed to fetch data')
-  //   }
-
-  //   const data = commentsSchema.parse(await res.json())
-
-  //   return data
-  // }
-
-  // const comments = await getComments()
-
-  const comments = [
-    {
-      id: '1',
-      user: 'test',
-      comment: 'test',
-    },
-    {
-      id: '2',
-      user: 'test',
-      comment: 'test',
-    },
-    {
-      id: '3',
-      user: 'test',
-      comment: 'test',
-    },
-  ]
+  // const comments = [
+  //   {
+  //     id: '1',
+  //     user: 'test',
+  //     comment: 'test',
+  //   },
+  //   {
+  //     id: '2',
+  //     user: 'test',
+  //     comment: 'test',
+  //   },
+  //   {
+  //     id: '3',
+  //     user: 'test',
+  //     comment: 'test',
+  //   },
+  // ]
 
   return (
     <main className="w-full max-w-lg flex flex-col gap-5 flex-1 items-center px-5 my-10">
