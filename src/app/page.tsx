18866a3d3cdd21f8d9a2e6e1681d04cfe0adf06a
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-05-26 15:26:26 -0300

    Fix: Cards Width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import { Contact } from './components/Contact'
 export default async function Home() {
   return (
     <div className="my-10 flex w-full flex-1 flex-col items-center justify-center overflow-x-clip px-5">
-      <div className="relative flex w-full max-w-7xl flex-col items-center rounded border-b border-purple-950 bg-purple-900 bg-[url(../assets/bg-stars.svg)] p-5 shadow-2xl shadow-purple-950">
+      <div className="relative flex w-full max-w-7xl flex-col items-center rounded  bg-gradient-conic from-purple-900 to-purple-900/80 p-5 shadow-2xl shadow-purple-950/80">
         {/* First Planet */}
 
         <div className="absolute right-0 top-0 -z-10 aspect-square w-2/3 max-w-md -translate-y-1/2 translate-x-1/2 rounded-full bg-orange-600 opacity-80 blur-3xl" />
@@ -27,19 +27,19 @@ export default async function Home() {
           // width={200}
           // height={200}
           alt="My profile photo"
-          className="absolute right-0 top-0 -z-10 aspect-square w-full max-w-xl -translate-y-1/2 translate-x-1/2 "
+          className="absolute right-0 top-0  -z-10 aspect-square w-full max-w-xl -translate-y-1/2 translate-x-1/2 transition-all"
         />
 
         {/* About me */}
-        <div className="relative z-10 flex flex-col items-center gap-5 bg-purple-900 bg-[url(../assets/bg-stars.svg)]">
+        <div className="relative z-10 flex flex-col items-center gap-5 ">
           <div className="absolute right-1/2 top-1/2 -z-10 h-full w-1/3 -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-80 blur-full" />
-          <Image
+          {/* <Image
             src={githubPhoto}
             width={200}
             height={200}
             alt="My profile photo"
             className="rounded-full"
-          />
+          /> */}
           <div className="flex max-w-xl flex-col gap-2 text-center">
             <h1 className="flex items-center justify-center text-xl font-bold">
               Hey, I&apos;m John 👨🏻‍🚀
@@ -81,7 +81,7 @@ export default async function Home() {
 
       {/* Skills */}
 
-      <div className="mt-5 flex w-full max-w-7xl flex-col items-center gap-2 rounded border-b border-zinc-500 bg-zinc-700/80 p-5 pt-5 text-zinc-100 shadow-md">
+      <div className="mt-5 flex w-full max-w-7xl flex-col items-center gap-2 rounded border-b border-zinc-500 bg-purple-900 p-5 pt-5 text-zinc-100 shadow-md">
         <h2 className="text-lg font-bold">Skills</h2>
         <div className="grid w-full  grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           <SkillCard name="HTML" SkillIcon={HTMLIcon} />
@@ -96,7 +96,7 @@ export default async function Home() {
 
       {/* Contact */}
 
-      <div className="mt-5 flex w-full max-w-7xl justify-center rounded border-b border-zinc-500 bg-zinc-700/80 p-5 pt-5 shadow-md">
+      <div className="mt-5 flex w-full max-w-7xl justify-center rounded border-b border-zinc-500 bg-purple-900 p-5 pt-5 shadow-md">
         <Contact />
       </div>
     </div>
