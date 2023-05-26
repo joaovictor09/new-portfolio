8fae32a7d10682e639d20c9cdd6a3583bfc3a87e
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-05-26 13:58:45 -0300

    Feat: First Planet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import NodeJsIcon from '../assets/Nodejs.svg'
 import ReactIcon from '../assets/React.svg'
 import TypescriptIcon from '../assets/Typescript.svg'
 import githubPhoto from '../assets/github-profile-image.jpeg'
+import planet from '../assets/planet.png'
 
 import { SkillCard } from './components/SkillCard'
 import { Contact } from './components/Contact'
@@ -18,29 +19,39 @@ export default async function Home() {
   return (
     <div className="my-10 w-full max-w-7xl flex-1 flex-col items-center px-5">
       {/* About me */}
-
-      <div className="flex flex-col items-center gap-5 rounded border-b border-zinc-500 bg-zinc-700/80 p-5 shadow-md">
+      <div className="relative flex flex-col items-center rounded border-b border-purple-950 bg-purple-900 bg-[url(../assets/bg-stars.svg)] p-5 shadow-2xl shadow-purple-950">
+        <div className="absolute right-0 top-0 -z-10 aspect-square w-2/3 max-w-md -translate-y-1/2 translate-x-1/2 rounded-full bg-orange-600 opacity-80 blur-3xl" />
         <Image
-          src={githubPhoto}
-          width={200}
-          height={200}
+          src={planet}
+          // width={200}
+          // height={200}
           alt="My profile photo"
-          className="rounded-full"
+          className="absolute right-0 top-0 -z-10 aspect-square w-full max-w-xl -translate-y-1/2 translate-x-1/2 "
         />
-        <div className="flex max-w-xl flex-col gap-2 text-center">
-          <h1 className="flex items-center justify-center text-xl font-bold">
-            Hey, I&apos;m John <div className="animate-wiggle"> 👋</div>
-          </h1>
-          <p className="text-zinc-400">
-            I&apos;m a software developer based in Criciúma, Santa Catarina. I
-            specialize in building (and occasionally designing) exceptional
-            websites, applications, and everything in between.
-          </p>
+        <div className="relative z-10 flex flex-col items-center gap-5 bg-purple-900 bg-[url(../assets/bg-stars.svg)]">
+          <div className="absolute right-1/2 top-1/2 -z-10 h-full w-1/3 -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-80 blur-full" />
+          <Image
+            src={githubPhoto}
+            width={200}
+            height={200}
+            alt="My profile photo"
+            className="rounded-full"
+          />
+          <div className="flex max-w-xl flex-col gap-2 text-center">
+            <h1 className="flex items-center justify-center text-xl font-bold">
+              Hey, I&apos;m John 👨🏻‍🚀
+            </h1>
+            <p className="text-zinc-400">
+              I&apos;m a software developer based in Criciúma, Santa Catarina. I
+              specialize in building (and occasionally designing) exceptional
+              websites, applications, and everything in between.
+            </p>
+          </div>
         </div>
 
         {/* Socials */}
 
-        <div className="flex items-center gap-2 text-zinc-400">
+        <div className="mt-2 flex items-center gap-2 text-zinc-400">
           <Link
             href="https://www.instagram.com/jooao.victor_/"
             className="transition-all hover:scale-110 hover:text-zinc-300"
