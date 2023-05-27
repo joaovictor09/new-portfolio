a0650b39b6733a62d44a6a302684b2957c06e277
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-05-27 14:52:27 -0300

    Fix: Can't resolve 'nodemailer'

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,50 +1,50 @@
-import { NextResponse } from 'next/server'
-import { z } from 'zod'
-import nodemailer from 'nodemailer'
+// import { NextResponse } from 'next/server'
+// import { z } from 'zod'
+// import nodemailer from 'nodemailer'
 
-const sendMessageFormSchema = z.object({
-  name: z
-    .string()
-    .nonempty('O nome é obrigatório')
-    .transform((name) => {
-      return name
-        .trim()
-        .split(' ')
-        .map((word) => {
-          return word[0].toLocaleUpperCase().concat(word.substring(1))
-        })
-        .join(' ')
-    }),
-  email: z
-    .string()
-    .nonempty('O e-mail é obrigatório')
-    .email('Formato de e-mail inválido'),
-  message: z.string().nonempty(),
-})
+// const sendMessageFormSchema = z.object({
+//   name: z
+//     .string()
+//     .nonempty('O nome é obrigatório')
+//     .transform((name) => {
+//       return name
+//         .trim()
+//         .split(' ')
+//         .map((word) => {
+//           return word[0].toLocaleUpperCase().concat(word.substring(1))
+//         })
+//         .join(' ')
+//     }),
+//   email: z
+//     .string()
+//     .nonempty('O e-mail é obrigatório')
+//     .email('Formato de e-mail inválido'),
+//   message: z.string().nonempty(),
+// })
 
-export async function POST(request: Request) {
-  const { email, message, name } = sendMessageFormSchema.parse(
-    await request.json(),
-  )
+// export async function POST(request: Request) {
+//   const { email, message, name } = sendMessageFormSchema.parse(
+//     await request.json(),
+//   )
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com',
-    port: 465,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  })
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.hostinger.com',
+//     port: 465,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   })
 
-  const emailSend = await transporter.sendMail({
-    from: 'contato@joaovictor09.dev',
-    to: 'contato@joaovictor09.dev',
-    replyTo: email,
-    subject: `Contato de ${name}`,
-    text: `Mensagem: ${message}`,
-  })
+//   const emailSend = await transporter.sendMail({
+//     from: 'contato@joaovictor09.dev',
+//     to: 'contato@joaovictor09.dev',
+//     replyTo: email,
+//     subject: `Contato de ${name}`,
+//     text: `Mensagem: ${message}`,
+//   })
 
-  console.log(emailSend)
+//   console.log(emailSend)
 
-  return NextResponse.json(emailSend)
-}
+//   return NextResponse.json(emailSend)
+// }
